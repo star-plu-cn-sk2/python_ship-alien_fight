388e7b0b45a61063151b97b8a1d7e7373acf7ffe
star-plu-cn-sk2 <31024176+star-plu-cn-sk2@users.noreply.github.com>
2017-11-24 11:33:17 +0800

    wd commit

--- txt_2_image.py
+++ txt_2_image.py
@@ -5,13 +5,11 @@ import StringIO
 import Image, ImageFont, ImageDraw
 import pygame
 pygame.init()
-#text = u'this is test txt, test 123.'
 f = open('output.txt','r')
-text = f.read()
-#im = Image.new("RGB", (300, 50), (255, 255, 255))
-im = Image.new("RGB", (400, 400), (255, 255, 255))
-#font = pygame.font.Font(os.path.join('fonts','simsun.ttc'),14)
-font = pygame.font.Font(os.path.join('fonts','simsun.ttc'),9)
+text = f.readlines()
+print text
+im = Image.new("RGB", (1024, 800), (255, 255, 255))
+font = pygame.font.Font(os.path.join('fonts','simsun.ttc'),12)
 rtext = font.render(text, True, (0, 0, 0), (255, 255, 255))
 sio = StringIO.StringIO()
 pygame.image.save(rtext, sio)
@@ -19,4 +17,5 @@ sio.seek(0)
 line = Image.open(sio)
 im.paste(line,(10, 5))
 im.show()
-im.save("t.png") 
+im.save("t.png")
+
